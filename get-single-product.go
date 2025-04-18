fa2be26ff1e7a755bd8169fbd76f48bee0b33f88
Ilya Ignatenko <ighnatenko@vk.com>
2025-04-18 22:38:04 +0300

    fix execution timeout

--- get-single-product.go
+++ get-single-product.go
@@ -128,6 +128,7 @@ func GetProductById(rw http.ResponseWriter, req *http.Request) {
 		l.Error().Stack().Msg("received from database product id is invalid")
 		rw.WriteHeader(http.StatusBadRequest)
 		rw.Write([]byte(`{"error": "product id is invalid"}`))
+
 		return
 	}
 
