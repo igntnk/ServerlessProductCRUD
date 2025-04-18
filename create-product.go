bc91e554466a990d498da0642130c7f1a2d584a4
Ilya Ignatenko <ighnatenko@vk.com>
2025-04-18 23:20:09 +0300

    ci fix

--- create-product.go
+++ create-product.go
@@ -91,6 +91,7 @@ func CreateProduct(rw http.ResponseWriter, req *http.Request) {
 		l.Error().Err(err).Msg("failed to fetch set of products")
 		rw.WriteHeader(http.StatusInternalServerError)
 		rw.Write([]byte(`{"error": "failed to fetch set of products"}`))
+
 		return
 	}
 
