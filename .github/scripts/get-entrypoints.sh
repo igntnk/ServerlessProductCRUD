ebc9348e02ce50886fc288d639a2ddb6fe0784a2
Ilya Ignatenko <ighnatenko@vk.com>
2025-04-18 23:13:35 +0300

    get entrypoint for every function

---
+++ .github/scripts/get-entrypoints.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+FILE_BASENAME=$(basename "$1" .go)
+
+ENTRYPOINT=$(yq -r ".$FILE_BASENAME" .github/entrypoints.yaml)
+
+if [ -z "$ENTRYPOINT" ] || [ "$ENTRYPOINT" == "null" ]; then
+  echo "Entrypoint not found for file: $FILE_BASENAME"
+  exit 1
+fi
+
+echo "entrypoint=$ENTRYPOINT" >> $GITHUB_OUTPUT
